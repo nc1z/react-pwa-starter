791bf9a2b3a57a9483a3b5931f67b910e1cebb74
nc1z <ncxwork@gmail.com>
2023-08-04 22:30:09 +0800

    :recycle: Removed auth modules

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,34 +1,19 @@
 import { createBrowserRouter } from "react-router-dom"
 
 import App from "../App"
-import ProtectedRoute from "./guards/ProtectedRoute"
 import Home from "../views/Home/Home"
-import Login from "../views/Login/Login"
 import NotFound from "../views/NotFound/NotFound"
 
 enum Route {
     ROOT = "/",
     HOME = "/home",
-    LOGIN = "/login",
     NOTFOUND = "*",
 }
 
-const guardedRoutes = [
-    {
-        element: <ProtectedRoute />,
-        children: [
-            {
-                path: Route.HOME,
-                element: <Home />,
-            },
-        ],
-    },
-]
-
 const publicRoutes = [
     {
-        path: Route.LOGIN,
-        element: <Login />,
+        path: Route.HOME,
+        element: <Home />,
     },
     {
         path: Route.NOTFOUND,
@@ -40,7 +25,7 @@ const routes = [
     {
         path: Route.ROOT,
         element: <App />,
-        children: [...guardedRoutes, ...publicRoutes],
+        children: [...publicRoutes],
     },
 ]
 
